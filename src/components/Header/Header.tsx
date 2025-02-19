af23dabb29cd346588e92d62f373708dd0b5e44f
KagnSeohyeon <shkang@coinplug.com>
2025-02-19 14:57:24 +0900

    next/image to img 태그로 변경

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,4 +1,7 @@
-import Image from "next/image";
+import arrowLeft from "../../assets/images/arrows_button_left__arrow.png";
+import highway from "../../assets/images/highway.png";
+import search from "../../assets/images/search__glass_search_magnifying__Streamline_Core.png";
+import menu from "../../assets/images/setting_menu_1__button_parallel_horizontal_lines_menu_navigation_three_hamburger__Streamline_Core.png";
 
 interface HeaderProps {
   onBack?: () => void;
@@ -12,21 +15,11 @@ const Header = ({ onBack, title = "경부고속도로", routeId }: HeaderProps) 
       <div className="flex items-center gap-4">
         {onBack && (
           <button onClick={onBack} className="p-2">
-            <Image
-              src="/src/assets/images/arrows_button_left__arrow.png"
-              alt="뒤로가기"
-              width={24}
-              height={24}
-            />
+            <img src={arrowLeft.src} alt="뒤로가기" width={24} height={24} />
           </button>
         )}
         <div className="relative w-8 h-8">
-          <Image
-            src="/src/assets/images/highway.png"
-            alt="고속도로"
-            width={32}
-            height={32}
-          />
+          <img src={highway.src} alt="고속도로" width={32} height={32} />
           <div className="absolute flex justify-center items-center top-[1.8px] left-0 w-8 h-8 font-medium text-[#FFF]">
             {routeId || ""}
           </div>
@@ -39,20 +32,10 @@ const Header = ({ onBack, title = "경부고속도로", routeId }: HeaderProps) 
 
       <div className="flex items-center gap-4">
         <button className="p-2">
-          <Image
-            src="/src/assets/images/search__glass_search_magnifying__Streamline_Core.png"
-            alt="검색"
-            width={24}
-            height={24}
-          />
+          <img src={search.src} alt="검색" width={24} height={24} />
         </button>
         <button className="p-2">
-          <Image
-            src="/src/assets/images/setting_menu_1__button_parallel_horizontal_lines_menu_navigation_three_hamburger__Streamline_Core.png"
-            alt="메뉴"
-            width={24}
-            height={24}
-          />
+          <img src={menu.src} alt="메뉴" width={24} height={24} />
         </button>
       </div>
     </header>
